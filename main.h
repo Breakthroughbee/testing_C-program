28c23e574d816d483da39630cf0b0e179351fc6c
Breakthrough <temitopefatoki658@ggmail.com>
2023-05-22 11:14:28 +0100

    Created directory test-02, a header file and a test-01.c file

---
+++ main.h
@@ -0,0 +1,7 @@
+#indef MAIN_H
+#def MAIN_H
+#include <stdio>
+
+
+
+#endif /* MINA_H */
---
+++ test-01.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int main () {
+
+int num1, num2, sum;
+
+// User inputs two integers
+printf("Enter two integer:");
+
+// Stores user inputs
+scanf("%d %d, num1, num2);
+
+// Calculate sum
+sum = num1 + num2;
+
+// Print sum
+printf("%d + %d = %d, num1 + num2 = sum);
+return 0;
+}
+
